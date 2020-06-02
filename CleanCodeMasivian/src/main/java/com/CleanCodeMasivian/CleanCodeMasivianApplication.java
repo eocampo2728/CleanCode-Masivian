d64b0000fb4b380c246a0bc50be27afa4a2de7ad
eocampo2728 <eduardo.ocampo2728@hotmail.com>
2020-06-02 10:12:33 -0500

    Structure

---
+++ CleanCodeMasivian/src/main/java/com/CleanCodeMasivian/CleanCodeMasivianApplication.java
@@ -0,0 +1,13 @@
+package com.CleanCodeMasivian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeMasivianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanCodeMasivianApplication.class, args);
+	}
+
+}
