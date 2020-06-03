c0489f6fe9114e8b03b886bd37b649a513a47fa6
eocampo2728 <eduardo.ocampo2728@hotmail.com>
2020-06-03 09:14:16 -0500

    Format and model

---
+++ CleanCodeMasivian/src/main/java/com/CleanCodeMasivian/services/user/UserServicesStub.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.CleanCodeMasivian.services.user;
+
+/**
+ *
+ * @author Eduardo Ocampo
+ */
+public class UserServicesStub {
+    
+}
